dec37409eb3bbc20b060cb0ec9107f8d17b7cb2a
Anderson Ocaña <86977326+Andru-1987@users.noreply.github.com>
2022-06-16 07:39:03 -0300

    Update nomina.py

--- TP_IBERTI/nomina.py
+++ TP_IBERTI/nomina.py
@@ -1,6 +1,37 @@
+from users import EmpleadoTemp
+from users import EmpleadoFijo
 
 class NominaSector():
   areas_disponibles = ["contabilidad","logistica","administracio","ventas"]
+  empleados = []
+
+  def add_empleado():
+  	"""Creacion de usuario base y carga a la nomina
+  	"""
+  	data = (input("Que empleado desea cargar?\nTemporal\tFijo")).lower()
+	nombre = (input("ingrese nombre\t")).lower()
+	apellido = (input("ingrese apellido\t")).lower()
+	dni = int(input("ingrese dni: \t"))
+	area = self.area 
+	puesto = (input("ingrese puesto\t")).lower() or None
+
+  	if(data == "fijo"):
+  		salario_base = float(input("Salario Base : ")) or None
+  		selector = (input("Cuenta con horas extras? SI or No")) or "n"
+  		
+  		empleado = EmpleadoFijo(nombre,dni,apellido,puesto,area)
+
+  		if (selector == "s" or selector =="si")
+	  		cant_horas = float(input("ingrese cantidad de horas \t")) or None
+	  		valor_hora = float(input("ingrese valor por hora \t")) or None
+	  		empleado.set_horas_extras(cant_horas,valor_hora)
+	  	
+
+  	else:
+  		empleado = EmpleadoTemp()
+
+  	empleados.append(empleado)
+  	return 1
 
   def __init__(self,area,jefe_area):
    
@@ -18,4 +49,9 @@ class NominaSector():
       print("Se ha cargado exitosamente")
 
 
+    
+
+
+
 sector = NominaSector("area", "Miguel")
+
